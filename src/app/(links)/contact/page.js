a81dbcfeb018b8091569d55598c239cc5a80bf14
Unlogical-Plug <himanshumehta720@gmail.com>
2025-01-15 20:10:02 +0530

    Update page.js

--- src/app/(links)/contact/page.js
+++ src/app/(links)/contact/page.js
@@ -22,10 +22,10 @@ const Content = () => {
           <p className="text-lg">
             Email us at:{" "}
             <a 
-              href="mailto:mail@terabox.tech" 
+              href="mailto:plugovate@gmail.com" 
               className="text-blue-600 dark:text-blue-400 hover:underline"
             >
-              mail@terabox.tech
+              plugovate@gmail.com
             </a>
           </p>
         </div>
