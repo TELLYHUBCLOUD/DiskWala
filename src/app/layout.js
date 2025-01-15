d2fe227245354373e029d82dc8acf3427b2604e3
Unlogical-Plug <himanshumehta720@gmail.com>
2025-01-15 20:59:12 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
-import Script from "next/script";
+ import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
