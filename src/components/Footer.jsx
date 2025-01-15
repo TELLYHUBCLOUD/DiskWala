32ab8941dc33ffa853c2293e82db9943adb05b6b
Unlogical-Plug <himanshumehta720@gmail.com>
2025-01-16 05:08:23 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ export default function Footer() {
       {/* Desktop View */}
       <div className="hidden lg:flex flex-col">
         {/* Footer */}
-        <footer className="bg-slate-900/50 backdrop-blur-sm border-t border-white/5">
+        <footer className="bg-white backdrop-blur-sm border-t border-white/5">
           <div className="max-w-7xl mx-auto px-8 py-6">
-            <nav className="flex flex-wrap justify-center gap-x-8 gap-y-4 text-sm text-white">
+            <nav className="flex flex-wrap justify-center gap-x-8 gap-y-4 text-sm text-black">
               {[
                 ["Privacy Policy", "/privacy"],
                 ["About Us", "/about"],
