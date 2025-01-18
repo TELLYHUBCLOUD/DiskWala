b6a88f9085a37b4deb089b9acdc416e85b1fc66a
Unlogical-Plug <himanshumehta720@gmail.com>
2025-01-19 01:10:29 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,21 @@ import Downloader from "@/components/Downloader";
 import Footer from "@/components/Footer";
 import { Header } from "@/components/Header";
 
+export const metadata = {
+  title: "PlayTerabox: Video Downloader, Player, Embed Videos, No login",
+  description:
+    "Play and download Terabox videos easily with PlayTerabox. Our tool offers embed videos, skip ads, no login, and just pure video enjoyment!",
+  keywords: ["terabox", "terabox video downloader online", "terabox direct download", "terabox downloader", "terabox online downloader", "terabox video downloader", "terabox player", "terabox online player", "terabox links", "terabox link converter", "terabox direct link", "terabox direct videos", "terabox direct files", "terabox bypass", "terabox link bypass", "terabox video downloader", "terabox video download", "terabox link downloader", "terabox downloader online"],
+  openGraph: {
+    title: "PlayTerabox: Video Downloader, Player, Embed Videos, No login",
+    description: "Play and download Terabox videos easily with PlayTerabox. Our tool offers embed videos, skip ads, no login, and just pure video enjoyment!",
+    type: "website",
+    url: "https://playterabox.com",
+    images: ["/og.jpg"],
+  },
+  robots: "index, follow",
+};
+
 export default function Home() {
   return (
     <>
