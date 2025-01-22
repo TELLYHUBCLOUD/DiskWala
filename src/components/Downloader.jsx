be821ada5668e4a12fe54879f5248ee4e0c453fd
Unlogical-Plug <himanshumehta720@gmail.com>
2025-01-22 18:05:17 +0530

    Update Downloader.jsx

--- src/components/Downloader.jsx
+++ src/components/Downloader.jsx
@@ -163,15 +163,7 @@ const Downloader = () => {
 {/* Ad Code */}
 <div id="ad-container" className="my-4">
   <div>
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-          aclib.runBanner({
-            zoneId: '9403226',
-          });
-        `,
-      }}
-    ></script>
+<script type="text/javascript" src="//cdn.runative-syndicate.com/sdk/v1/bi.js" data-ts-spot="39faec82ab9542afa76743729ca802b0" data-ts-width="300" data-ts-height="100"  async defer></script>
   </div>
 </div>
 {/* End of Ad Code */}
