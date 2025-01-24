6ec1837e87f3fb507b9fa765453bfcff2e38efe6
Unlogical-Plug <himanshumehta720@gmail.com>
2025-01-24 15:47:01 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import Footer from "@/components/Footer";
 import { Header } from "@/components/Header";
 
 export const metadata = {
-  title: "PlayTerabox: Video Downloader, Player, Embed Videos, No login",
+  title: "PlayTerabox: Video Downloader, Player, 100% Working ✔️ No login",
   description:
-    "Play and download Terabox videos easily with PlayTerabox. Our tool offers embed videos, skip ads, no login, and just pure video enjoyment!",
+    "Play and download Terabox videos easily with PlayTerabox. Our tool is 100% Working and offers embed videos, skip ads, no login, and just pure video enjoyment!",
   keywords: ["terabox", "terabox video downloader online", "terabox direct download", "terabox downloader", "terabox online downloader", "terabox video downloader", "terabox player", "terabox online player", "terabox links", "terabox link converter", "terabox direct link", "terabox direct videos", "terabox direct files", "terabox bypass", "terabox link bypass", "terabox video downloader", "terabox video download", "terabox link downloader", "terabox downloader online"],
   openGraph: {
     title: "PlayTerabox: Video Downloader, Player, Embed Videos, No login",
@@ -21,15 +21,6 @@ export default function Home() {
   return (
     <>
       <Header />
-      {/* Ad Code */}
-<div id="ad-container" className="my-4">
-  <div
-    dangerouslySetInnerHTML={{
-      __html: `<script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>`,
-    }}
-  ></div>
-</div>
-{/* End of Ad Code */}
       <main className="mt-10">
         <Downloader />
       </main>
