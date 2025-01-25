b46779a258bab0ef0e9a786415dc77b72733c560
Unlogical-Plug <himanshumehta720@gmail.com>
2025-01-25 19:44:36 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,15 @@ export default function Home() {
   return (
     <>
       <Header />
+                {/* Ad Code */}
+<div id="ad-container" className="my-4">
+  <div
+    dangerouslySetInnerHTML={{
+      __html: `<script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('theetheks.com',8844627,document.createElement('script'))</script>`,
+    }}
+  ></div>
+</div>
+{/* End of Ad Code */}
       <main className="mt-10">
         <Downloader />
       </main>
